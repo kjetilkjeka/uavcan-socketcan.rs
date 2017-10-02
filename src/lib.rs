3c6e2ecb851affec5a5e7ba987bce996171b76a6
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-02 22:24:26 +0200

    Updated driver to new version of TransferInterface, also use FullTransferID as filter again

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,8 @@ impl CanInterface {
 
 impl TransferInterface for CanInterface {
     type Frame = CanFrame;
-
+    type IDContainer = Vec<FullTransferID>;
+    
     fn transmit(&self, frame: &Self::Frame) -> Result<(), TransmitError> {
         match self.interface.write_frame(&(*frame).into()) {
             Ok(()) => Ok(()),
@@ -44,18 +45,18 @@ impl TransferInterface for CanInterface {
         }
     }
 
-    fn receive(&self, identifier: Option<&FullTransferID>) -> Option<Self::Frame> {
-        if identifier.is_some() {
-            unimplemented!("No support for receive by identifier yet");
-        }
-        match self.interface.read_frame().ok() {
-            Some(frame) => Some(frame.into()),
-            None => None,
-        }
+    fn receive(&self, identifier: &FullTransferID) -> Option<Self::Frame> {
+        self.update_receive_buffer();
+        let data = self.rx_buffer.lock().unwrap();
+        let mut buffer = data.borrow_mut();
+        buffer.remove(identifier)
     }
 
-    fn received_completely(&self) -> &[FullTransferID] {
-        unimplemented!()
+    fn completed_receives(&self, identifier: FullTransferID, mask: FullTransferID) -> Self::IDContainer {
+        self.update_receive_buffer();
+        let data = self.rx_buffer.lock().unwrap();
+        let buffer = data.borrow();
+        buffer.completed_transfers(identifier, mask)
     }
 
 }
@@ -145,7 +146,7 @@ impl TransferBuffer{
 
 
 pub struct ReceiveBuffer {
-    map: HashMap<TransferFrameID, TransferBuffer>,
+    map: HashMap<FullTransferID, TransferBuffer>,
 }
 
 impl ReceiveBuffer {
@@ -154,11 +155,11 @@ impl ReceiveBuffer {
     }
 
     pub fn insert(&mut self, frame: CanFrame) {
-        self.map.entry(frame.id()).or_insert(TransferBuffer::new());
-        self.map.get_mut(&frame.id()).unwrap().push(frame);
+        self.map.entry(frame.full_id()).or_insert(TransferBuffer::new());
+        self.map.get_mut(&frame.full_id()).unwrap().push(frame);
     }
 
-    pub fn remove(&mut self, key: &TransferFrameID) -> Option<CanFrame> {
+    pub fn remove(&mut self, key: &FullTransferID) -> Option<CanFrame> {
         let (can_frame, empty) = { 
             let transfer_buffer = match self.map.get_mut(key) {
                 Some(x) => x,
@@ -174,12 +175,12 @@ impl ReceiveBuffer {
         Some(can_frame)
     }
 
-    pub fn completed_transfers(&self, identifier: TransferFrameID, mask: TransferFrameID) -> Vec<TransferFrameID> {
+    pub fn completed_transfers(&self, identifier: FullTransferID, mask: FullTransferID) -> Vec<FullTransferID> {
         self.map.iter()
             .filter(|&(key, _value)| key.mask(mask) == identifier.mask(mask))
             .filter(|&(_key, value)| value.is_complete())
             .map(|(key, _value)| key.clone())
-            .collect::<Vec<TransferFrameID>>()            
+            .collect::<Vec<FullTransferID>>()            
     }
 }
 
